08b73fb757430e298213cca113ac75f951c7ef64
paulnta <paul.ntawuruhunga@heig-vd.ch>
2018-10-04 17:48:40 +0200

    final version

---
+++ js/app.js
@@ -0,0 +1,123 @@
+// https://medium.freecodecamp.org/environment-settings-in-javascript-apps-c5f9744282b6
+const baseUrl =  window.location.hostname === 'localhost'
+  ? 'http://localhost:3000'
+  : 'https://heig-vd-ga-server.herokuapp.com';
+
+
+const defaultSearch = 'paulnta';
+const searchForm = document.getElementById('search-form');
+let chart = null;
+
+function getUser(username) {
+  return fetch(`${baseUrl}/users/${username}`)
+    .then(res => res.json());
+}
+
+function getLanguages(username) {
+  return fetch(`${baseUrl}/languages/${username}`)
+    .then(res => res.json());
+}
+
+function getGithubColors() {
+  return fetch('/data/github-colors.json')
+    .then(res => res.json());
+}
+
+function updateChart({ labels, data, backgroundColor }) {
+  const chartLanguages = document.getElementById('chart-languages');
+  const ctx = chartLanguages.getContext('2d');
+  const options = {
+    type: 'bar',
+    data: {
+      labels,
+      datasets: [{
+        data,
+        backgroundColor,
+      }],
+    },
+    options: {
+      legend: {
+        display: false
+      },
+      scales: {
+        xAxes: [{
+          ticks: {
+            fontFamily: "'Roboto Mono'",
+            fontSize: 12,
+          },
+          gridLines: {
+            display: false,
+          }
+        }],
+        yAxes: [{
+          ticks: {
+            fontFamily: "'Roboto Mono'",
+          }
+        }]
+      },
+    }
+  }
+
+  if (!chart) {
+    chart = new Chart(ctx, options);
+  } else {
+    chart.data.labels = options.data.labels;
+    chart.data.datasets = options.data.datasets;
+    chart.update();
+  }
+}
+
+function updateProfile(user) {
+  const avatar = document.getElementById('user-avatar');
+  const name = document.getElementById('user-name');
+  const login = document.getElementById('user-login');
+  avatar.src = user.avatar_url;
+  avatar.alt = `avatar of ${user.name}`;
+  name.innerHTML = user.name;
+  login.innerHTML = user.login;
+}
+
+function updatePlaceholder(content, className = 'text-secondary') {
+  const placeholder = document.getElementById('placeholder');
+  placeholder.className = className;
+  placeholder.innerHTML = content;
+}
+
+function handleSearch(username) {
+  updatePlaceholder('Loading...');
+
+  return Promise.all([
+    getUser(username),
+    getLanguages(username),
+    getGithubColors(),
+  ])
+    .then(([user, languages, colors]) => {
+      updatePlaceholder('');
+
+      const labels = Object.keys(languages);
+      const data = labels.map(label => languages[label]);
+      const backgroundColor = labels.map(label => {
+        const color = colors[label] ? colors[label].color : null
+        return color || '#000';
+      })
+
+      updateProfile(user);
+      updateChart({ labels, data, backgroundColor });
+    })
+    .catch(err => {
+      updatePlaceholder('Oups, an error occured. Sorry, this app sucks...', 'text-error');
+      console.error('Cannot fetch data', err)
+    })
+}
+
+searchForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const username = this.elements['username'].value;
+  if (!username) {
+    return;
+  }
+  handleSearch(username);
+});
+
+handleSearch(defaultSearch);
+
